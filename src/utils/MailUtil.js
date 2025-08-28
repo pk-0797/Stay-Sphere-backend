909a2fd7fad0a771d18ddcb02fc37448858c2103
Prahlad Kumar <prahladkumar1401@gmail.com>
2025-08-28 14:20:43 +0530

    mail

--- src/utils/MailUtil.js
+++ src/utils/MailUtil.js
@@ -1,20 +1,50 @@
+// const mailer = require("nodemailer");
+
+// const sendingMail = async (to, subject, text) => {
+//   try {
+//     const transporter = mailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: "stayspherewelcome@gmail.com",
+//         pass: "xlwo fmdv bawv vaft",
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: "stayspherewelcome@gmail.com",
+//       to: to,
+//       subject: subject,
+//       text: text,
+//     };
+
+//     const mailresponse = await transporter.sendMail(mailOptions);
+//     console.log("Mail sent successfully:", mailresponse);
+//     return mailresponse;
+//   } catch (error) {
+//     console.error("Error sending mail:", error);
+//     return { success: false, message: "Failed to send email" };
+//   }
+// };
+// module.exports = {
+//   sendingMail,
+// };
 const mailer = require("nodemailer");
 
-const sendingMail = async (to, subject, text) => {
+const sendingMail = async (to, subject, htmlContent) => {
   try {
     const transporter = mailer.createTransport({
       service: "gmail",
       auth: {
         user: "stayspherewelcome@gmail.com",
-        pass: "xlwo fmdv bawv vaft",
+        pass: "xlwo fmdv bawv vaft", // <-- app password
       },
     });
 
     const mailOptions = {
-      from: "stayspherewelcome@gmail.com",
-      to: to,
-      subject: subject,
-      text: text,
+      from: '"Stay Sphere" <stayspherewelcome@gmail.com>', // nicer sender name
+      to,
+      subject,
+      html: htmlContent, // ✅ use html instead of text
     };
 
     const mailresponse = await transporter.sendMail(mailOptions);
@@ -25,6 +55,7 @@ const sendingMail = async (to, subject, text) => {
     return { success: false, message: "Failed to send email" };
   }
 };
+
 module.exports = {
   sendingMail,
 };
